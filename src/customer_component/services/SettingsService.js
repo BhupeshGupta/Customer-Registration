b8c9bafea8f4812ac4587268d9939281b1247dda
rvsingh011 <rvsingh011@gmail.com>
2016-08-24 11:52:40 +0530

    production env added in packet.json

--- src/customer_component/services/SettingsService.js
+++ src/customer_component/services/SettingsService.js
@@ -1,9 +1,9 @@
 export default class SettingService {
     constructor() {}
     erpServerUrl() {
-        return 'http://localhost:8080';
+        return 'http://erp.arungas.com';
     }
     pythonServerUrl() {
-        return 'http://localhost:9005';
+        return 'http://localhost:9006';
     }
 }
